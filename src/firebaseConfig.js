39d5a018c1e6661803e8669b985a1041689abb19
jvseds <jvaaltonen@gmail.com>
2024-12-18 14:05:40 +0200

    changed firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+// import dotenv from "dotenv";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,6 +12,17 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+// dotenv.config();
+// const firebaseConfig = {
+//   apiKey: process.env.VITE_FIREBASE_API_KEY,
+//   authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.VITE_FIREBASE_APP_ID,
+//   measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+// };
 
 const app = initializeApp(firebaseConfig);
 
