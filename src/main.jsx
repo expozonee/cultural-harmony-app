bad873c264e920e4abc80a6fc89ee57300ac3ea2
Ibrahem <boss.abrahem@hotmail.com>
2024-12-18 10:34:00 +0200

    updated the vite settings to handle impoting env variables

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY;
+console.log(PUBLISHABLE_KEY);
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
