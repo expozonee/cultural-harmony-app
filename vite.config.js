ecc5de04d880fb2e28f8007023b8131d9a371094
Ibrahem <boss.abrahem@hotmail.com>
2024-12-18 12:28:46 +0200

    settings update

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,13 @@ dotenv.config();
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "VITE_");
+  // const env = loadEnv(mode, process.cwd(), "VITE_");
   return {
     // vite config
     plugins: [react()],
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
-      "process.env": JSON.stringify(env),
+      "process.env": JSON.stringify(process.env),
     },
   };
 });
