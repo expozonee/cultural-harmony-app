1d1409e69699fea0a09387d2300a864a0d82ba7e
Ibrahem <boss.abrahem@hotmail.com>
2024-12-18 15:56:52 +0200

    added dashboard section to the admin page and added the logic to handle displaying the desired list of either events or users

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,3 +1,40 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router";
+import UsersList from "../../components/Admin/UsersList";
+import EventsList from "../../components/Admin/EventsList";
+import DashboardButton from "../../components/Admin/DashboardButton";
+
+const BUTTONS_TYPES = ["Events", "Users"];
+
 export default function AdminPage() {
-  return <div>AdminPage</div>;
+  const [params] = useSearchParams();
+  const dashboardParams = params.get("dashboard");
+
+  const [dashboardSection, setDashboardSection] = useState(
+    dashboardParams ?? "events"
+  );
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <hr />
+      <div className="flex dashboard__buttons">
+        {BUTTONS_TYPES.map((b, index) => {
+          return (
+            <DashboardButton
+              key={index}
+              name={b}
+              onClick={() => {
+                setDashboardSection(b.toLowerCase());
+              }}
+              isActive={b.toLowerCase() === dashboardSection}
+            />
+          );
+        })}
+      </div>
+      <section>
+        {dashboardSection === "events" ? <EventsList /> : <UsersList />}
+      </section>
+    </div>
+  );
 }
