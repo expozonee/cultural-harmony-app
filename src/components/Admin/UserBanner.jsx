aafd97b95255ed9ec5c2054858df260aabdf3c1a
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-18 14:48:26 +0200

    add UserBanner

--- src/components/Admin/UserBanner.jsx
+++ src/components/Admin/UserBanner.jsx
@@ -1,3 +1,15 @@
 export default function UserBanner() {
-  return <div>UserBanner</div>;
+  return (
+    <div className="flex align-center justify-center">
+      <img
+        className="user-img"
+        src="/boliviainteligente-wIBDrEv73xY-unsplash.jpg"
+        alt=""
+      />
+      <section className="user-details">
+        <h1>Hello, Ward!</h1>
+        <p>email</p>
+      </section>
+    </div>
+  );
 }
