52898e8d456f138a72f3239572448a0faeb04aeb
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-18 21:45:25 +0200

    Add EventList component for rendering individual event rows

--- src/components/Events/EventList.jsx
+++ src/components/Events/EventList.jsx
@@ -1,3 +1,11 @@
-export default function EventList() {
-  return <div>EventList</div>;
+export default function EventList({ data }) {
+  return (
+    <tbody>
+      <tr>
+        <td>{data.event_title}</td>
+        <td>{data.location}</td>
+        <td>{data.date}</td>
+      </tr>
+    </tbody>
+  );
 }
