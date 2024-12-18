cc68ad9e25f25276677fa4905d4ffbe6c3dafffb
Ibrahem <boss.abrahem@hotmail.com>
2024-12-18 12:19:22 +0200

    fixed typo

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ dotenv.config();
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "Vite_");
+  const env = loadEnv(mode, process.cwd(), "VITE_");
   return {
     // vite config
     plugins: [react()],
