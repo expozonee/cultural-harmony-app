6b5809bd89e04801fd6606a1a48ac69d4f9132a2
israel litvak <lisrael1989@gmail.com>
2024-12-18 11:20:34 +0200

    fix bugs with connection to firebase data

--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-import { collection, getDocs, QuerySnapshot } from "firebase/firestore";
-import { db } from "../firebaseConfig.js";
-import EventPreview from "./EventPreview";
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebaseConfig.js';
+import EventPreview from './EventPreview';
 
 function EventsList() {
   const [events, setEvents] = useState([]);
@@ -9,31 +9,21 @@ function EventsList() {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const queryEventsDB = await getDocs(collection(db, "events"));
-        console.log("query snapshot: ", queryEventsDB);
+        const queryEventsDB = await getDocs(collection(db, 'events'));
 
-        const eventsData = QuerySnapshot.docs.map((doc) => ({
+        const eventsData = queryEventsDB.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
 
-        setEvents(eventsData.data);
+        setEvents(eventsData);
       } catch (error) {
-        console.log("Error fetching data from firestore: ", error);
+        console.error('Error fetching data from firestore: ', error);
       }
     };
 
     fetchEvents();
   }, []);
-
-  // useEffect(() => {
-  //   fetch("/public/data/events.json")
-  //     .then((response) => response.json())
-  //     .then((data) => setEvents(data))
-  //     .catch((error) => {
-  //       console.error("Error fetching event: ", error);
-  //     });
-  // }, []);
 
   return (
     <>
