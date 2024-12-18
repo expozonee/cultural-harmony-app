12e16cc5c3fc7954e2957b51dc8bee170986c788
jvseds <jvaaltonen@gmail.com>
2024-12-18 09:51:50 +0200

    still needs fixing to QuerySnapshot

--- public/data/data.js
+++ public/data/data.js
@@ -1,22 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
-  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../src/firebase.js";
 
 const eventsData = [
   {
@@ -87,5 +70,4 @@ async function saveEventsData(events) {
   }
 }
 
-// Call the function to save the JSON data
 saveEventsData(eventsData);
--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -1,16 +1,39 @@
 import { useEffect, useState } from "react";
+import { collection, getDocs, QuerySnapshot } from "firebase/firestore";
+import { db } from "../firebase.js";
 import EventPreview from "./EventPreview";
 
 function EventsList() {
   const [events, setEvents] = useState([]);
+
   useEffect(() => {
-    fetch("/public/data/events.json")
-      .then((response) => response.json())
-      .then((data) => setEvents(data))
-      .catch((error) => {
-        console.error("Error fetching event: ", error);
-      });
+    const fetchEvents = async () => {
+      try {
+        const queryEventsDB = await getDocs(collection(db, "events"));
+        console.log("query snapshot: ", queryEventsDB);
+
+        const eventsData = QuerySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setEvents(eventsData.data);
+      } catch (error) {
+        console.log("Error fetching data from firestore: ", error);
+      }
+    };
+
+    fetchEvents();
   }, []);
+
+  // useEffect(() => {
+  //   fetch("/public/data/events.json")
+  //     .then((response) => response.json())
+  //     .then((data) => setEvents(data))
+  //     .catch((error) => {
+  //       console.error("Error fetching event: ", error);
+  //     });
+  // }, []);
 
   return (
     <>
