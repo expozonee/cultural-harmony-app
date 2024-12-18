24e74146b7e765c35206308f78fbd54a5eb297cb
Ibrahem <boss.abrahem@hotmail.com>
2024-12-18 12:16:42 +0200

    installed dotenv package

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "Vite_");
   return {
     // vite config
     plugins: [react()],
