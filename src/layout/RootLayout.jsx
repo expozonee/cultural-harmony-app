30bab6175b190a8474c6150b58fdf291a7cc6605
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-19 12:39:34 +0200

    add ClerkProvider

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,11 +1,25 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import NavBar from "../components/NavBar/NavBar";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 export function RootLayout() {
+  const navigate = useNavigate();
   return (
-    <div>
+    <ClerkProvider
+      routerPush={(to) => navigate(to)}
+      routerReplace={(to) => navigate(to, { replace: true })}
+      publishableKey={PUBLISHABLE_KEY}
+    >
       <NavBar />
-      <Outlet />
-    </div>
+      <section className="flex justify-center">
+        <Outlet />
+      </section>
+    </ClerkProvider>
   );
 }
