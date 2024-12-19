b1e3870e6d6153e02ed1890bfc5fa3ffeee8de1e
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-19 12:42:21 +0200

    add path for signin and signup pages

--- src/router/RouterProvider.jsx
+++ src/router/RouterProvider.jsx
@@ -1,6 +1,8 @@
 import { HomePage } from "../pages/HomePage";
 import { RootLayout } from "../layout/RootLayout";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import SignInPage from "./../pages/LoginPage/SignIn";
+import SignUpPage from "./../pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: "sign-up/*",
+        element: <SignUpPage />,
+      },
+      {
+        path: "sign-in/*",
+        element: <SignInPage />,
       },
     ],
   },
