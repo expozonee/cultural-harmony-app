99597c018dfe58d7f667c4e7805a8d0e51311b9b
Ibrahem <boss.abrahem@hotmail.com>
2024-12-19 12:49:30 +0200

    updated the to url path to reflect the update to the admin dashboard url structure

--- src/components/Admin/DashboardButton.jsx
+++ src/components/Admin/DashboardButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export default function DashboardButton({ name, onClick, isActive }) {
   return (
-    <Link to={`?dashboard=${name.toLowerCase()}`}>
+    <Link to={`/admin/${name.toLowerCase()}`}>
       <button style={{ color: `${isActive ? "red" : ""}` }} onClick={onClick}>
         {name}
       </button>
