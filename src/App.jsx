97092c6d1cd10cc2c2f106ec7c40965d874eadf1
Ibrahem <boss.abrahem@hotmail.com>
2024-12-21 12:20:42 +0200

    added the user provider to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import "./assets/main.css";
+import { UserContextProvider } from "./context/UserContext";
 import { RouterProviderComponent as RouterProvider } from "./router/RouterProvider";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 function App() {
-  return <RouterProvider />;
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <UserContextProvider>
+        <RouterProvider />;
+      </UserContextProvider>
+    </ClerkProvider>
+  );
 }
 
 export default App;
