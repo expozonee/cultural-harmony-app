1d2f8eaf86216e66e742ec4548a7226cfd6f72bc
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-23 11:46:27 +0200

    get all the data of JoinedEvents and put the data in table

--- src/components/User/UserJoinedEvents/UserJoinedEvents.jsx
+++ src/components/User/UserJoinedEvents/UserJoinedEvents.jsx
@@ -1,34 +1,65 @@
 import { useEffect, useState } from "react";
 import { useUserData } from "../../../context/UserContext";
+import UserJoinedEventData from "./UserJoinedEventData";
 
 export default function UserJoinedEvents() {
-  const { getUserJoinedEvents, unJoinEvent } = useUserData();
+  const { getUserJoinedEvents, unJoinEvents } = useUserData();
   const [joinedEvents, setJoinedEvents] = useState([]);
+  const [checkedIndices, setCheckedIndices] = useState([]);
 
   useEffect(() => {
     async function getJoinedEvents() {
       const joinedEvents = await getUserJoinedEvents();
-
       setJoinedEvents(joinedEvents);
     }
 
     getJoinedEvents();
   }, [getUserJoinedEvents]);
 
+  const handleCheckboxChange = (index) => {
+    setCheckedIndices((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
+    );
+  };
+
   return (
-    <div>
+    <>
       {joinedEvents && joinedEvents.length > 0 ? (
-        joinedEvents.map((e, i) => {
-          return (
-            <div key={i}>
-              <p>Event: {e.event_title}</p>
-              <button onClick={() => unJoinEvent(e.id)}>Unjoin Event</button>
-            </div>
-          );
-        })
+        <>
+          <table className="table-event" border="1">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Location</th>
+                <th>Date</th>
+                <th>Host</th>
+              </tr>
+            </thead>
+            <tbody>
+              {joinedEvents.map((e, index) => (
+                <UserJoinedEventData
+                  key={index}
+                  index={index}
+                  onCheckboxChange={handleCheckboxChange}
+                  data={e}
+                />
+              ))}
+            </tbody>
+          </table>
+          <button
+            onClick={() =>
+              unJoinEvents(
+                checkedIndices.map((index) => joinedEvents[index].id)
+              )
+            }
+          >
+            Delete Selected
+          </button>
+        </>
       ) : (
         <p>You did not join any events yet!</p>
       )}
-    </div>
+    </>
   );
 }
