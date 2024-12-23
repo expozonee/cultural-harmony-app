7679829b0515a10eb162ce439c4133d1777df722
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-23 11:38:18 +0200

    add the table body

---
+++ src/components/User/UserEventsCreated/UserCreatedEventData.jsx
@@ -0,0 +1,13 @@
+export default function UserCreatedEventData({ data }) {
+  return (
+    <tr>
+      <td>
+        <input type="checkbox" />
+      </td>
+      <td>{data.event_title}</td>
+      <td>{data.location.city_name}</td>
+      <td>{data.date}</td>
+      <td>{data.event_host_name}</td>
+    </tr>
+  );
+}
