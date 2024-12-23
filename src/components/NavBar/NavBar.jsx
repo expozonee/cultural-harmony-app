976bce1133e52442d3b10e1a604d30c56d8eb2f9
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-23 11:42:53 +0200

    check if the user is signed in to show him the Dashboard

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, UserButton, useUser } from "@clerk/clerk-react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "/icons8-people-working-together-100.png";
 import { useState } from "react";
@@ -6,6 +6,7 @@ import { useState } from "react";
 export default function NavBar() {
   const { pathname } = useLocation();
   const [currentPage, setCurrentPage] = useState(pathname);
+  const { isSignedIn } = useUser();
 
   return (
     <header className="app-header">
@@ -35,6 +36,19 @@ export default function NavBar() {
                 Events
               </Link>
             </li>
+            {isSignedIn && (
+              <li>
+                <Link
+                  style={{
+                    backgroundColor: `${currentPage === "/admin" ? "red" : ""}`,
+                  }}
+                  to="/admin"
+                  onClick={() => setCurrentPage("/admin")}
+                >
+                  Dashboard
+                </Link>
+              </li>
+            )}
             <li>
               <Link to="/" onClick={() => setCurrentPage("about-us")}>
                 About Us
