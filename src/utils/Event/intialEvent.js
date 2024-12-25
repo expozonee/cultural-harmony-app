ed4c42a2e3920d5431553c9d02729db21d07bc2f
Ibrahem <boss.abrahem@hotmail.com>
2024-12-25 18:56:19 +0200

    added initial event obejct and moved the initAutocomplete fn to new file

---
+++ src/utils/Event/intialEvent.js
@@ -0,0 +1,15 @@
+export const intialEvent = {
+  event_title: "",
+  summary: "",
+  date: "",
+  event_start_time: "",
+  description: "",
+  imgUrl: "",
+  location: {
+    lat: undefined,
+    lng: undefined,
+    city_name: "",
+  },
+  max_participants: "",
+  contribution_list: [],
+};
