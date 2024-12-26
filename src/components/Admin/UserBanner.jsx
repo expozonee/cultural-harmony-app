d7c906acba12d22d404649a26376cb62cc930b24
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-12-26 11:00:57 +0200

    add the image for the user

--- src/components/Admin/UserBanner.jsx
+++ src/components/Admin/UserBanner.jsx
@@ -2,14 +2,11 @@ import { useUser } from "@clerk/clerk-react";
 
 export default function UserBanner() {
   const user = useUser();
+  console.log(user);
 
   return (
     <div className="flex align-center justify-center">
-      <img
-        className="user-img"
-        src="/boliviainteligente-wIBDrEv73xY-unsplash.jpg"
-        alt=""
-      />
+      <img className="user-img" src={user.user.imageUrl} alt="" />
       <section className="user-details">
         <h1>Hello, {user.user?.fullName}!</h1>
         <p>{user.user?.emailAddresses[0].emailAddress}</p>
