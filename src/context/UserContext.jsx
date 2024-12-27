6e39c71beda8111f42042fe4aef1c476820e7bf4
Ibrahem <boss.abrahem@hotmail.com>
2024-12-28 00:19:13 +0200

    passed the userData to the provider value

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -56,6 +56,7 @@ export function UserContextProvider({ children }) {
     <UserContext.Provider
       value={{
         users,
+        userData,
         getUserJoinedEvents,
         getUserCreatedEvents,
         joinEvent,
