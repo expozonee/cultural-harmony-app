551da3f2820dbae72d60a6710b0d3c662db76e5b
Ibrahem <boss.abrahem@hotmail.com>
2024-12-28 00:15:53 +0200

    removed the log statement

--- src/components/Home/FeaturedEvents.jsx
+++ src/components/Home/FeaturedEvents.jsx
@@ -2,24 +2,20 @@ import EventPreview from "../EventPreview";
 import { useLoaderData } from "react-router";
 
 export default function FeaturedEvents() {
-    const events = useLoaderData();
-    console.log("upcomingEvents: ", events);
+  const events = useLoaderData();
 
-    
-    return (
-        <section className="featured-events">
-            <h2 className="featured-events-title">Featured Events</h2>
-            <ul className="events-list">
-                {events.map((event) => (
-                    <li className="event-preview" key={event.id}>
-                        <div>
-                            <EventPreview event={event} />
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </section>
-    );
-
-
+  return (
+    <section className="featured-events">
+      <h2 className="featured-events-title">Featured Events</h2>
+      <ul className="events-list">
+        {events.map((event) => (
+          <li className="event-preview" key={event.id}>
+            <div>
+              <EventPreview event={event} />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
