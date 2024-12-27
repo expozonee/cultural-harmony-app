5ec4c71e29ae07072feff965ad41eb2de5dd4d79
Ibrahem <boss.abrahem@hotmail.com>
2024-12-27 22:58:22 +0200

    added the form component to the page and created a loader for the page to fetch the desired event

--- src/pages/UpdateEventPage/UpdateEventPage.jsx
+++ src/pages/UpdateEventPage/UpdateEventPage.jsx
@@ -1,3 +1,24 @@
+/* eslint-disable react-refresh/only-export-components */
+import { useLoaderData } from "react-router-dom";
+import { getEvents } from "../../firebase/utils/getEvents";
+import AddEventForm from "../AddEventPage/AddEventForm";
+
+export async function getEventByIdLoader(req) {
+  const {
+    params: { eventId },
+  } = req;
+
+  const event = (await getEvents()).find((e) => e.id === eventId);
+
+  return event;
+}
+
 export default function UpdateEventPage() {
-  return <div>UpdateEventPage</div>;
+  const { id, ...data } = useLoaderData();
+
+  return (
+    <>
+      <AddEventForm eventData={data} eventId={id} />
+    </>
+  );
 }
