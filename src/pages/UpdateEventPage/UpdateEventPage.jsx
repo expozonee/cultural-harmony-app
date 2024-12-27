c0ca823c20535e55bce5deb2a0abf3ba7f8b1ae6
Ibrahem <boss.abrahem@hotmail.com>
2024-12-28 00:21:39 +0200

    fetched the from the url the search params to determine the the location the user came from

--- src/pages/UpdateEventPage/UpdateEventPage.jsx
+++ src/pages/UpdateEventPage/UpdateEventPage.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useSearchParams } from "react-router-dom";
 import { getEvents } from "../../firebase/utils/getEvents";
 import AddEventForm from "../AddEventPage/AddEventForm";
+import { useUserData } from "../../context/UserContext";
+import { Link } from "react-router-dom";
 
 export async function getEventByIdLoader(req) {
   const {
@@ -14,11 +16,28 @@ export async function getEventByIdLoader(req) {
 }
 
 export default function UpdateEventPage() {
+  const [params] = useSearchParams();
+  const from = params.get("from");
+
+  const { userData } = useUserData();
+  const currentEmail = userData?.email;
   const { id, ...data } = useLoaderData();
+  const isHost = data.host_email_address === currentEmail;
+
+  if (!isHost)
+    return (
+      <>
+        <h2>You are not allowed to enter this page!</h2>
+        <p>Only the event host can edit the event.</p>
+        <Link to={"/events"}>
+          <button>Go to Events</button>
+        </Link>
+      </>
+    );
 
   return (
     <>
-      <AddEventForm eventData={data} eventId={id} />
+      <AddEventForm eventData={data} eventId={id} from={from} />
     </>
   );
 }
