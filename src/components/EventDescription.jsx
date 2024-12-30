d8b5a6743b24abfeba14ef38e080f73eaed31bf5
Ibrahem <boss.abrahem@hotmail.com>
2024-12-30 09:54:07 +0200

    connected the fetch event logic with the events context

--- src/components/EventDescription.jsx
+++ src/components/EventDescription.jsx
@@ -1,87 +1,93 @@
-import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router";
-import { db } from "../firebase/firebaseConfig";
 import { useUserData } from "../context/UserContext";
 import ContributionList from "./ContributionList";
-import { useNavigate } from "react-router-dom";
-import { useUser } from "@clerk/clerk-react";
-import { updateEventParticipants } from "../utils/updateEvent";
 import { Link, Outlet } from "react-router";
 import Poll from "./Poll";
+import { useEvents } from "../context/EventsContext";
 
 function EventDescription() {
   const { eventId } = useParams();
-  const { userData } = useUserData();
+  const { userData, unJoinEvents, joinEvent } = useUserData();
+  const { getEvenyById } = useEvents();
   const currentEmail = userData?.email;
 
   const location = useLocation();
-  const [event, setEvent] = useState(location.state?.event || null);
+  const [event, setEvent] = useState(undefined);
   const [loading, setLoading] = useState(!location.state?.event);
-  const navigate = useNavigate();
-  const { user } = useUser();
 
-  console.log("Event:", event);
-  const hasJoined = event?.participants?.includes(user?.username);
+  const hasJoined = event?.participants?.includes(userData?.email);
 
   useEffect(() => {
-    const fetchEvent = async () => {
-      if (!event) {
-        try {
-          const docRef = doc(db, "events", eventId);
-          const docSnap = await getDoc(docRef);
+    // const fetchEvent = async () => {
+    //   if (!event) {
+    //     try {
+    //       const docRef = doc(db, "events", eventId);
+    //       const docSnap = await getDoc(docRef);
 
-          if (docSnap.exists()) {
-            const eventData = { id: docSnap.id, ...docSnap.data() };
-            setEvent(eventData);
-            console.log("Fetched event:", eventData);
-          } else {
-            console.error("Event not found");
-          }
-        } catch (error) {
-          console.error("Error fetching event from Firestore:", error);
-        } finally {
-          setLoading(false);
-        }
+    //       if (docSnap.exists()) {
+    //         const eventData = { id: docSnap.id, ...docSnap.data() };
+    //         setEvent(eventData);
+    //         console.log("Fetched event:", eventData);
+    //       } else {
+    //         console.error("Event not found");
+    //       }
+    //     } catch (error) {
+    //       console.error("Error fetching event from Firestore:", error);
+    //     } finally {
+    //       setLoading(false);
+    //     }
+    //   }
+    // };
+
+    async function getEvent() {
+      try {
+        setLoading(true);
+        const eventData = await getEvenyById(eventId);
+        setEvent(eventData);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
       }
-    };
-
-    fetchEvent();
-  }, [event, eventId]);
-
-  const handleParticipantAction = async (action) => {
-    if (!user) {
-      navigate("/sign-in", { state: { from: location.pathname } });
-      return;
     }
 
-    try {
-      const update = await updateEventParticipants(
-        db,
-        eventId,
-        user.username,
-        action
-      );
-      const updatedParticipants = update[0];
-      const updatedContributionList = update[1];
+    getEvent();
+  }, [eventId, getEvenyById]);
 
-      setEvent((prevEvent) => ({
-        ...prevEvent,
-        participants: updatedParticipants,
-        contribution_list: updatedContributionList,
-      }));
+  // const handleParticipantAction = async (action) => {
+  //   if (!user) {
+  //     navigate("/sign-in", { state: { from: location.pathname } });
+  //     return;
+  //   }
 
-      console.log(
-        `User ${action === "join" ? "joined" : "unjoined"} the event:`,
-        user.username
-      );
-    } catch (error) {
-      console.error(
-        `Error ${action === "join" ? "joining" : "unjoining"} event:`,
-        error
-      );
-    }
-  };
+  //   try {
+  //     const update = await updateEventParticipants(
+  //       db,
+  //       eventId,
+  //       user.username,
+  //       action
+  //     );
+  //     const updatedParticipants = update[0];
+  //     const updatedContributionList = update[1];
+
+  //     setEvent((prevEvent) => ({
+  //       ...prevEvent,
+  //       participants: updatedParticipants,
+  //       contribution_list: updatedContributionList,
+  //     }));
+
+  //     console.log(
+  //       `User ${action === "join" ? "joined" : "unjoined"} the event:`,
+  //       user.username
+  //     );
+  //   } catch (error) {
+  //     console.error(
+  //       `Error ${action === "join" ? "joining" : "unjoining"} event:`,
+  //       error
+  //     );
+  //   }
+  // };
 
   if (loading) return <p>Loading...</p>;
   if (!event) return <p>Event not found!</p>;
@@ -109,24 +115,22 @@ function EventDescription() {
       {hasJoined ? (
         <button
           className="unjoin-button"
-          onClick={() => handleParticipantAction("unjoin")}
+          onClick={() => unJoinEvents([eventId])}
         >
           Unjoin Event
         </button>
       ) : (
-        <button
-          className="join-button"
-          onClick={() => handleParticipantAction("join")}
-        >
+        <button className="join-button" onClick={() => joinEvent(eventId)}>
           Join Event
         </button>
       )}
       {hasJoined && (
         <div className="event-contribution-list">
           <ContributionList
-            eventDocId={String(event.id)}
+            eventId={eventId}
             contributionList={event.contribution_list || []}
             setEvent={setEvent}
+            eventData={event}
           />
         </div>
       )}
@@ -141,7 +145,7 @@ function EventDescription() {
         </div>
       )}
 
-      {event.event_host_name === user?.username ? (
+      {event.event_host_name === userData?.email ? (
         <Link to={`create-poll`}>
           <button>Create A Poll for This Event</button>
         </Link>
