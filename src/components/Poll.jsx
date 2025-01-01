e0f55e63946e0c2ed151bd420a74e9b4fff2e299
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2025-01-01 18:03:17 +0200

    add 2 buttons (remove vote & remove poll)

--- src/components/Poll.jsx
+++ src/components/Poll.jsx
@@ -1,18 +1,15 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
-// import { db } from "../firebase/firebaseConfig";
-// import { doc, updateDoc } from "firebase/firestore";
-// import { useEvents } from "../context/EventsContext";
 import { useUserData } from "../context/UserContext";
 import { usePoll } from "../hooks/usePoll";
 import { PollData } from "../utils/Poll";
 
 function Poll({ poll }) {
   const { eventId } = useParams();
-  // const { getEventById } = useEvents();
   const { userData } = useUserData();
   const [selectedOption, setSelectedOption] = useState(null);
-  const [isUserVoted, vote, removeVote] = usePoll(eventId, poll);
+  const [isUserVoted, vote, removeVote, removePoll] = usePoll(eventId, poll);
+  const [changeVote, setChangeVote] = useState(false);
   const pollData = new PollData(poll);
 
   useEffect(() => {
@@ -28,51 +25,16 @@ function Poll({ poll }) {
     setSelectedOption(e.target.value);
   }
 
-  // async function handleSubmission(e) {
-  //   e.preventDefault();
-  //   // alert if the user didn't vote in the poll - encourage their engagement
-  //   if (!selectedOption) {
-  //     alert("please answer the poll");
-  //     return;
-  //   }
-  //   // increase the number of votes for this specific option by 1
-  //   // poll.voted[selectedOption] = (poll.votes[selectedOption] || 0) + 1;
+  async function handleRemovePoll(e) {
+    e.preventDefault();
+    await removePoll();
+  }
 
-  //   const evenRef = doc(db, "events", eventId);
-  //   console.log(evenRef);
-
-  //   const { id, ...eventData } = await getEventById(eventId);
-
-  //   const pollToUpdate = eventData.polls.find(
-  //     (p) => p.question === poll.question
-  //   );
-  //   const updatedPoll = {
-  //     ...pollToUpdate,
-  //     options: pollToUpdate.options.map((option) => {
-  //       if (option.question_name !== selectedOption) return option;
-  //       return {
-  //         ...option,
-  //         votes_count: option.votes_count + 1,
-  //         voted_users: [...option.voted_users, userData.email],
-  //       };
-  //     }),
-  //   };
-
-  //   const newPolls = eventData.polls.filter(
-  //     (p) => p.question !== poll.question
-  //   );
-
-  //   newPolls.push(updatedPoll);
-
-  //   console.log(newPolls);
-
-  //   const updatedEventData = {
-  //     ...eventData,
-  //     polls: newPolls,
-  //   };
-
-  //   await updateDoc(evenRef, updatedEventData);
-  // }
+  async function handleChangeVote(e) {
+    e.preventDefault();
+    await removeVote();
+    setChangeVote(!changeVote);
+  }
 
   async function handleRemoveVote(e) {
     e.preventDefault();
@@ -81,10 +43,9 @@ function Poll({ poll }) {
 
   async function handleSubmit(e) {
     e.preventDefault();
+
     await vote(selectedOption);
   }
-
-  console.log(pollData.totalVotes);
 
   return (
     <>
@@ -97,12 +58,23 @@ function Poll({ poll }) {
               value={option.question_name}
               checked={selectedOption === option.question_name}
               onChange={handleOptionSelection}
+              disabled={isUserVoted && !changeVote}
             />
             {option.question_name}
+
+            <p>
+              {pollData.getVotesCountByOptionName(option.question_name)} /
+              {pollData.totalVotes}
+            </p>
           </label>
         ))}
-        <button type="submit">{isUserVoted ? "Update Vote" : "Vote!"}</button>
+        {isUserVoted && !changeVote ? (
+          <button onClick={handleChangeVote}>Change Vote</button>
+        ) : (
+          <button type="submit">Vote</button>
+        )}
         <button onClick={handleRemoveVote}>Remove vote</button>
+        <button onClick={handleRemovePoll}>Remove Poll</button>
       </form>
     </>
   );
