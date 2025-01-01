b57723602fa977d46ac9216278f01353751f0f49
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2025-01-01 18:06:12 +0200

    change optionsTotalVotes to getVotesCountByOptionName

--- src/utils/Poll.js
+++ src/utils/Poll.js
@@ -6,12 +6,9 @@ export function PollData(poll) {
     return total + option.votes_count;
   }, 0);
 
-  this.optionsTotalVotes = () => {
-    return poll.options.map((option) => {
-      return {
-        optionName: option.question_name,
-        votesCount: option.votes_count,
-      };
-    });
+  this.getVotesCountByOptionName = (optionName) => {
+    return poll.options.find((option) => {
+      return option.question_name === optionName;
+    }).votes_count;
   };
 }
