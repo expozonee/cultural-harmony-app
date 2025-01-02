72b12c2108ef259b545ec60612bd5c0147e55a06
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2025-01-02 15:51:50 +0200

    add LoadScript to fix the looding screen

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,42 +1,16 @@
 import { Outlet } from "react-router";
 import NavBar from "../components/NavBar/NavBar";
-// import { useUser } from "@clerk/clerk-react";
-// import { useEffect } from "react";
-// import { getUsers } from "../firebase/utils/getUsers";
-// import { addUserToDb } from "../firebase/utils/addUserToDb";
+import { LoadScript } from "@react-google-maps/api";
 
 export function RootLayout() {
-  // const { isSignedIn, user } = useUser();
-
-  // useEffect(() => {
-  //   if (!isSignedIn) return;
-
-  //   async function handleSignedInUser() {
-  //     const users = await getUsers();
-  //     const userEmailAddress = user.primaryEmailAddress.emailAddress;
-  //     const isUserInDb = users.find((u) => u.email === userEmailAddress);
-
-  //     if (!isUserInDb) {
-  //       const userToAdd = {
-  //         email: userEmailAddress,
-  //         profileImage: user.imageUrl,
-  //         eventsJoined: [],
-  //         eventsOrganized: [],
-  //         isAdmin: false,
-  //         name: user.fullName,
-  //       };
-
-  //       const result = await addUserToDb(userToAdd);
-  //       console.log(result);
-  //     }
-  //   }
-  //   handleSignedInUser();
-  // }, [user, isSignedIn]);
-
   return (
     <>
-      <NavBar />
-      <Outlet />
+      <LoadScript
+        googleMapsApiKey={import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY}
+      >
+        <NavBar />
+        <Outlet />
+      </LoadScript>
     </>
   );
 }
