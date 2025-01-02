0b561a84c35801fdc73b306fb80433f8b4c05e7d
jvseds <jvaaltonen@gmail.com>
2025-01-02 23:33:40 +0200

    added logic to block users from using free prompt for unrelated topics

--- src/components/ChatInputBox.jsx
+++ src/components/ChatInputBox.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-function ChatInputBox({ loading, onSend }) {
+function ChatInputBox({ loading, onSend, eventDetails }) {
   const [input, setInput] = useState("");
 
   const handleSubmit = (e) => {
--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import ChatWindow from "./ChatWindow";
 import ChatInputBox from "./ChatInputBox";
+
 function Chatbot({ eventDetails }) {
   const geminyApiKey = import.meta.env.VITE_GEMINI_API_KEY;
   const geminiModel = "gemini-1.5-flash";
@@ -16,8 +17,50 @@ function Chatbot({ eventDetails }) {
     `advise me concisely how to properly dress for this event: ${eventDetails.event_title}`,
   ];
 
-  const sendMessage = async (userInput) => {
+  // free user prompt validation
+  const validatePrompt = async (userInput) => {
     try {
+      const genAI = new GoogleGenerativeAI(geminyApiKey);
+      const model = genAI.getGenerativeModel({ model: geminiModel });
+
+      const validationPrompt = ```here is a given prompt: ${userInput}. if it refers directly to this event: ${eventDetails.event_title} which is described as: ${eventDetails.description}, then return javasctipt true, else return false.```;
+
+      const validationFlag = await model.generateContent(validationPrompt);
+      const validationResult = validationFlag.response
+        .text()
+        .toLowerCase()
+        .includes("true");
+      return validationResult;
+    } catch (error) {
+      console.error("Error while validating prompt: ", error);
+      return false;
+    }
+  };
+
+  const sendMessage = async (userInput) => {
+    setLoading(true);
+    try {
+      // check if the prompt is a built-in one or user's free prompt
+      const isBuilInPrompt = builtInPrompts.includes(userInput);
+
+      // if it's a user's free prompt, check if it's a valid one
+      if (!isBuilInPrompt) {
+        const isValid = await validatePrompt(userInput);
+        if (!isValid) {
+          setConversation((prevConversation) => [
+            // if the prompt is invalid, show an error message to the user
+            ...prevConversation,
+            { sender: "user", message: userInput },
+            {
+              sender: "bot",
+              message:
+                "I can only provide data that refers to this event. Please ask me something which is related to the event.",
+            },
+          ]);
+          return;
+        }
+      }
+
       const genAI = new GoogleGenerativeAI(geminyApiKey);
       const model = genAI.getGenerativeModel({ model: geminiModel });
 
