0e99d6891c6fbbe2e24923552d596ddaca619d86
Ibrahem <boss.abrahem@hotmail.com>
2025-01-07 16:15:55 +0200

    added logic to handle to going to the user dashboard without signing in

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,7 +1,30 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 import UserBanner from "../components/Admin/UserBanner";
+import { useUser } from "@clerk/clerk-react";
 
 export default function AdminLayout() {
+  const { isSignedIn } = useUser();
+
+  if (!isSignedIn) {
+    return (
+      <article
+        style={{
+          maxWidth: "1000px",
+          marginInline: "auto",
+          display: "grid",
+          justifyContent: "center",
+          textAlign: "center",
+          marginTop: "5rem",
+        }}
+      >
+        <h2>Sign in first to access the user dashboard</h2>
+        <Link to={"/sign-in"}>
+          <button>Sign In</button>
+        </Link>
+      </article>
+    );
+  }
+
   return (
     <div className="admin-layout">
       <UserBanner />
